300b7f24ef0bdb6de99a55278c90f4dc76db6325
unclepeach <jingtao.t@gmail.com>
2018-06-25 15:38:09 -0700

    Update MericsModule.py
    Groupby+Resample+tailsdata for ratio calculation

--- MericsModule.py
+++ MericsModule.py
@@ -1,3 +1,4 @@
+### preparation
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -14,7 +15,7 @@ df['Deal - Deal created'] = pd.to_datetime(df['Deal - Deal created'])
 df.index = df['Deal - Deal created'] 
 ###del df['Deal - Deal created']
 
-###1.Leads last week/month
+### leads trends plot
 df['Deal - Deal created'].resample('W').count()
 weekly_leads = df['Deal - Deal created'].resample('W').count()
 weekly_leads.plot()
@@ -23,6 +24,27 @@ monthly_leads = df['Deal - Deal created'].resample('M').count()
 monthly_leads.plot()
 
 ### period setting and use
-###begin = pd.Timestamp('2018-06-18 07:00:00')
-###end = pd.Timestamp('2018-06-24 23:00:00')
-###df[(df.index >= begin)&(df.index <=end)]
+#begin = pd.Timestamp('2018-06-18 07:00:00')
+#end = pd.Timestamp('2018-06-24 23:00:00')
+#df[(df.index >= begin)&(df.index <=end)]
+
+###1.Leads last week/month
+leads_w = df['Deal - Deal created'].resample('W').count().tail(1).get(0)
+print ('Leads_last week =', leads_w)
+leads_m = df['Deal - Deal created'].resample('M').count().tail(1).get(0)
+print ('Leads_last month =', leads_m)
+
+###2.Conversion ratio last week/month
+dfa = df.groupby(['Deal - Status']).get_group('Won')
+won_w = dfa['Deal - Deal created'].resample('W').count().tail(1)
+won_w = won_w.get(0)
+all_w = df['Deal - Deal created'].resample('W').count().tail(1)
+all_w = all_w.get(0)
+print ('Conversion ratio_last week = '+'{:.1%}'.format(won_w/all_w))
+
+dfa = df.groupby(['Deal - Status']).get_group('Won')
+won_m = dfa['Deal - Deal created'].resample('M').count().tail(1)
+won_m = won_m.get(0)
+all_m = df['Deal - Deal created'].resample('M').count().tail(1)
+all_m = all_m.get(0)
+print ('Conversion ratio_last month = '+'{:.1%}'.format(won_m/all_m))
